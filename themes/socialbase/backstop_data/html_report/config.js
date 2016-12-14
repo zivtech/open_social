9e6fa247444e2a08a335ad1d4c1bb0d48f85ec1f
Peter Polman <info@peterpolman.nl>
2016-12-14 14:50:23 +0100

    DS-964 - Started test implementation BackstopJS.

---
+++ themes/socialbase/backstop_data/html_report/config.js
@@ -0,0 +1,136 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_Open_Social_Homepage_0_navbar_0_phone.png",
+        "test": "../bitmaps_test/20161214-090159/prod_test_Open_Social_Homepage_0_navbar_0_phone.png",
+        "selector": ".navbar",
+        "fileName": "prod_test_Open_Social_Homepage_0_navbar_0_phone.png",
+        "label": "Open Social Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -50
+          },
+          "misMatchPercentage": "39.33",
+          "analysisTime": 62,
+          "getDiffImage": null
+        },
+        "diffImage": "../bitmaps_test/20161214-090159/failed_diff_prod_test_Open_Social_Homepage_0_navbar_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_Open_Social_Homepage_1_block-socialsaas-views-block-activity-stream-block-stream-homepage_0_phone.png",
+        "test": "../bitmaps_test/20161214-090159/prod_test_Open_Social_Homepage_1_block-socialsaas-views-block-activity-stream-block-stream-homepage_0_phone.png",
+        "selector": "#block-socialsaas-views-block-activity-stream-block-stream-homepage",
+        "fileName": "prod_test_Open_Social_Homepage_1_block-socialsaas-views-block-activity-stream-block-stream-homepage_0_phone.png",
+        "label": "Open Social Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "3.50",
+          "analysisTime": 552,
+          "getDiffImage": null
+        },
+        "diffImage": "../bitmaps_test/20161214-090159/failed_diff_prod_test_Open_Social_Homepage_1_block-socialsaas-views-block-activity-stream-block-stream-homepage_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_Open_Social_Homepage_0_navbar_1_tablet_v.png",
+        "test": "../bitmaps_test/20161214-090159/prod_test_Open_Social_Homepage_0_navbar_1_tablet_v.png",
+        "selector": ".navbar",
+        "fileName": "prod_test_Open_Social_Homepage_0_navbar_1_tablet_v.png",
+        "label": "Open Social Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -50
+          },
+          "misMatchPercentage": "43.99",
+          "analysisTime": 44,
+          "getDiffImage": null
+        },
+        "diffImage": "../bitmaps_test/20161214-090159/failed_diff_prod_test_Open_Social_Homepage_0_navbar_1_tablet_v.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_Open_Social_Homepage_1_block-socialsaas-views-block-activity-stream-block-stream-homepage_1_tablet_v.png",
+        "test": "../bitmaps_test/20161214-090159/prod_test_Open_Social_Homepage_1_block-socialsaas-views-block-activity-stream-block-stream-homepage_1_tablet_v.png",
+        "selector": "#block-socialsaas-views-block-activity-stream-block-stream-homepage",
+        "fileName": "prod_test_Open_Social_Homepage_1_block-socialsaas-views-block-activity-stream-block-stream-homepage_1_tablet_v.png",
+        "label": "Open Social Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "2.70",
+          "analysisTime": 1254,
+          "getDiffImage": null
+        },
+        "diffImage": "../bitmaps_test/20161214-090159/failed_diff_prod_test_Open_Social_Homepage_1_block-socialsaas-views-block-activity-stream-block-stream-homepage_1_tablet_v.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_Open_Social_Homepage_0_navbar_2_tablet_h.png",
+        "test": "../bitmaps_test/20161214-090159/prod_test_Open_Social_Homepage_0_navbar_2_tablet_h.png",
+        "selector": ".navbar",
+        "fileName": "prod_test_Open_Social_Homepage_0_navbar_2_tablet_h.png",
+        "label": "Open Social Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 88,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_Open_Social_Homepage_1_block-socialsaas-views-block-activity-stream-block-stream-homepage_2_tablet_h.png",
+        "test": "../bitmaps_test/20161214-090159/prod_test_Open_Social_Homepage_1_block-socialsaas-views-block-activity-stream-block-stream-homepage_2_tablet_h.png",
+        "selector": "#block-socialsaas-views-block-activity-stream-block-stream-homepage",
+        "fileName": "prod_test_Open_Social_Homepage_1_block-socialsaas-views-block-activity-stream-block-stream-homepage_2_tablet_h.png",
+        "label": "Open Social Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.19",
+          "analysisTime": 644,
+          "getDiffImage": null
+        },
+        "diffImage": "../bitmaps_test/20161214-090159/failed_diff_prod_test_Open_Social_Homepage_1_block-socialsaas-views-block-activity-stream-block-stream-homepage_2_tablet_h.png"
+      },
+      "status": "fail"
+    }
+  ]
+});
