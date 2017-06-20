56a43fde9ad303e7b8659770681b48dafd04c7b9
Peter Polman <info@peterpolman.nl>
2017-06-20 10:47:28 +0200

    DS-3230 - Fixed navbar-search error when enabling bigpipe.

--- modules/social_features/social_core/js/navbar-search.js
+++ modules/social_features/social_core/js/navbar-search.js
@@ -9,7 +9,10 @@
 
     Drupal.behaviors.navbarSearch = {
         attach: function (context, settings) {
-            new UISearch(document.getElementById('navbar-search'));
+
+            $(document).ready(function(){
+              new UISearch(document.getElementById('navbar-search'));
+            });
 
             $('#content').on('click', function(){
               $('.navbar-toggle:not(.collapsed)').click();
